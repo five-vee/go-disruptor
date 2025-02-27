2f9ae52c4c39fc39937dc2e0937dcde3e2002943
five-vee <8535086+five-vee@users.noreply.github.com>
2025-02-27 23:13:15 +0000

    Benchmark against other disruptor port(s)

--- benchmarks/benchmark_test.go
+++ benchmarks/benchmark_test.go
@@ -4,17 +4,21 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/five-vee/disruptor"
+	fivevee "github.com/five-vee/disruptor"
+	smartystreets "github.com/smartystreets-prototypes/go-disruptor"
 )
 
 type testData struct{ _ [16]byte }
 
-func BenchmarkDisruptor_1_65536(b *testing.B) {
-	sp, err := disruptor.NewSingleProducerBuilder[testData]().
-		WithSize(1 << 16).
+func BenchmarkDisruptor_1_20(b *testing.B) {
+	sp, err := fivevee.NewSingleProducerBuilder[testData]().
+		WithSize(1 << 20).
 		Build()
 	if err != nil {
 		b.Fatalf("NewSingleProducerBuilder.Build() failed: %v", err)
+	}
+	for range 1 << 19 {
+		sp.Produce(testData{})
 	}
 	b.ResetTimer()
 	var wg sync.WaitGroup
@@ -28,15 +32,57 @@ func BenchmarkDisruptor_1_65536(b *testing.B) {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		for range b.N {
+		for range (1 << 19) + b.N {
 			_ = sp.Consume()
 		}
 	}()
 	wg.Wait()
 }
 
-func BenchmarkChannel_1_65536(b *testing.B) {
-	c := make(chan testData, 1<<16)
+// consumer to be used by the smartystreets disruptor.
+type smartystreetsConsumer struct {
+	mask       int64
+	ringBuffer []testData
+}
+
+func (c smartystreetsConsumer) Consume(lower, upper int64) {
+	for ; lower <= upper; lower++ {
+		_ = c.ringBuffer[lower&c.mask]
+	}
+}
+
+func BenchmarkSmartystreets_1_20(b *testing.B) {
+	ringBuffer := make([]testData, 1<<20)
+	mask := int64((1 << 20) - 1)
+	disruptor := smartystreets.New(
+		smartystreets.WithCapacity(1<<20),
+		smartystreets.WithConsumerGroup(smartystreetsConsumer{mask, ringBuffer}),
+	)
+	b.ResetTimer()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for range b.N {
+			sequence := disruptor.Reserve(1)
+			ringBuffer[sequence&mask] = testData{}
+			disruptor.Commit(sequence, sequence)
+		}
+		_ = disruptor.Close()
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		disruptor.Read()
+	}()
+	wg.Wait()
+}
+
+func BenchmarkChannel_1_20(b *testing.B) {
+	c := make(chan testData, 1<<20)
+	for range 1 << 19 {
+		c <- testData{}
+	}
 	b.ResetTimer()
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -49,19 +95,22 @@ func BenchmarkChannel_1_65536(b *testing.B) {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		for range b.N {
+		for range (1 << 19) + b.N {
 			_ = <-c
 		}
 	}()
 	wg.Wait()
 }
 
-func BenchmarkDisruptor_4_65536(b *testing.B) {
-	mp, err := disruptor.NewMultiProducerBuilder[testData]().
-		WithSize(1 << 16).
+func BenchmarkDisruptor_4_20(b *testing.B) {
+	mp, err := fivevee.NewMultiProducerBuilder[testData]().
+		WithSize(1 << 20).
 		Build()
 	if err != nil {
 		b.Fatalf("NewMultiProducerBuilder.Build() failed: %v", err)
+	}
+	for range 1 << 19 {
+		mp.Produce(testData{})
 	}
 	b.ResetTimer()
 	var wg sync.WaitGroup
@@ -84,8 +133,11 @@ func BenchmarkDisruptor_4_65536(b *testing.B) {
 	wg.Wait()
 }
 
-func BenchmarkChannel_4_65536(b *testing.B) {
-	c := make(chan testData, 1<<16)
+func BenchmarkChannel_4_20(b *testing.B) {
+	c := make(chan testData, 1<<20)
+	for range 1 << 19 {
+		c <- testData{}
+	}
 	b.ResetTimer()
 	var wg sync.WaitGroup
 	for range 4 {
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/five-vee/disruptor
 go 1.24.0
 
 require github.com/google/go-cmp v0.7.0
+
+require github.com/smartystreets-prototypes/go-disruptor v0.0.0-20231024205940-61200af675a1 // indirect
