755298bb018745f0943a233eaf0f591648062bd3
five-vee <8535086+five-vee@users.noreply.github.com>
2025-03-02 17:58:40 +0000

    Add a very basic package comment

--- disruptor.go
+++ disruptor.go
@@ -1,3 +1,8 @@
+// Package disruptor provides an implementation of the LMAX Disruptor.
+//
+// If for some reason you have Go code that needs to process messages at
+// sub-microsecond latency, where shaving every nanosecond counts, then
+// consider the disruptor pattern.
 package disruptor
 
 import (
